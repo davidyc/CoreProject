dcda6683dc484df082af29a81b4ac3f6c65366f1
Sergey Davydov <Sergey_Davydov2@EPKZKARW0882>
2020-12-16 23:33:29 +0600

    move to postgres

--- CoreMVCProject/CoreProject/Startup.cs
+++ CoreMVCProject/CoreProject/Startup.cs
@@ -27,7 +27,7 @@ namespace CoreProject
         {
             services.AddControllersWithViews();
             services.AddDbContext<MSSQLTestContext>(options =>
-               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+               options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
