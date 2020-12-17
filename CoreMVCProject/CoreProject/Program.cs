74e386fc67e00d9eaf06598a3593b9a9dbb26b56
Sergey Davydov <Sergey_Davydov2@EPKZKARW0882>
2020-12-17 11:26:41 +0600

    add host ken to start

--- CoreMVCProject/CoreProject/Program.cs
+++ CoreMVCProject/CoreProject/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace CoreProject
@@ -21,6 +22,16 @@ namespace CoreProject
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseKestrel(opts =>
+                    {
+                        // Bind directly to a socket handle or Unix socket
+                        // opts.ListenHandle(123554);
+                        // opts.ListenUnixSocket("/tmp/kestrel-test.sock");
+                        opts.Listen(IPAddress.Loopback, port: 5002);
+                        opts.ListenAnyIP(5003);
+                        opts.ListenLocalhost(5004, opts => opts.UseHttps());
+                        opts.ListenLocalhost(5005, opts => opts.UseHttps());
+                    });
                 });
     }
 }
