a409d8812694db90cdaf9d7270f2ce8e6ec2cef0
Sergey Davydov <Sergey_Davydov2@EPKZKARW0882>
2020-12-17 16:47:42 +0600

    change ui elements for view home? about

--- CoreMVCProject/CoreProject/Controllers/HomeController.cs
+++ CoreMVCProject/CoreProject/Controllers/HomeController.cs
@@ -13,12 +13,12 @@ namespace CoreProject.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly MSSQLTestContext _db;
+     //   private readonly MSSQLTestContext _db;
 
-        public HomeController(ILogger<HomeController> logger, MSSQLTestContext context)
+        public HomeController(ILogger<HomeController> logger)//, MSSQLTestContext context)
         {
             _logger = logger;
-            _db = context;
+           // _db = context;
         }
 
         public IActionResult Index()
@@ -26,7 +26,7 @@ namespace CoreProject.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
